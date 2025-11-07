62b77e597d5d84d555de26b678dbe03e4644aedb
MindfulMuse <sadpri67@gmail.com>
2025-11-07 12:15:34 +0530

    redeploy

---
+++ app/(protected)/admin/schedule/nurse/page.tsx
@@ -0,0 +1,12 @@
+// app/(protected)/admin/schedule/nurse/page.tsx
+
+import React from 'react';
+
+export default function NurseSchedulePage() {
+  return (
+    <div>
+      <h1>Nurse Schedule</h1>
+      {/* Your content here */}
+    </div>
+  );
+}
